8fc255c27ca37d3553b3da82ab6b8fda4c5b6d19
HaulerM <haulermusa18@gmail.com>
2024-09-12 13:12:32 +0300

    Add files via upload

---
+++ hauler/Python( Variables)/Basic/Data types.py
@@ -0,0 +1,16 @@
+#DATA TYPES
+#INTENGERS
+age=19
+mark=95
+temprature=10
+# Strings
+name="Musa haula"
+message="how are you"
+#floats
+weight=49.6
+average_mark=50.7
+#boolean
+is_active=false
+is_completed=true
+
+#Finding the data type
---
+++ hauler/Python( Variables)/Basic/variables.py
@@ -0,0 +1,21 @@
+#VARIABLES
+
+#Naming Conventions
+#Snake Case
+student_name = " Musa Haulah "
+student_age  =  19
+student_course = " Computer Science "
+student_weight =49.9
+
+
+print(student_name)
+print(student_age)
+print(student_course)
+
+#strings
+print("My name is "+ student_name )
+print( "My age is " + str (student_age))
+print("My weight is " + str(student_weight))
+
+
+
